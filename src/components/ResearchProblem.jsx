883274294d502dd120b1bce9882eab497d5033e8
sajani <it20117696@my.sliit.lk>
2023-11-02 06:09:04 +0530

    final commit

--- src/components/ResearchProblem.jsx
+++ src/components/ResearchProblem.jsx
@@ -43,7 +43,7 @@ const ResearchProblem = () => {
               <iframe
                 width="100%"
                 height="350"
-                src="https://www.youtube.com/embed/YqeW9_5kURI?si=u6n_yDH4t7-P4jE2&amp;start=1"
+                src="https://www.youtube.com/embed/KEMm_jbemcA?si=s33tg4wdya3MB4qK"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
